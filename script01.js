db3a351c0f135fe77b44e5fdc562fc705d136b76
Jsantanabs <theilonmota@gmail.com>
2025-04-22 21:04:26 -0300

    Mudança Nos operadores

--- script01.js
+++ script01.js
@@ -3,7 +3,10 @@ function generateNumber(){
     const min = Math.ceil(document.querySelector(".first-input").value)
     const max = Math.floor(document.querySelector(".second-input").value)
 
-    const result = Math.floor(Math.random() * (max - min) + min);
-    
-    alert(`O número gerado foi: ${result}`)
+    if(min >= max) {
+        alert("O valor mínimo deve ser menor que o valor máximo.")
+    } else{
+        const result = Math.floor(Math.random() * (max - min + 1)) + min
+        alert(`O número gerado foi: ${result}`)
+    }
 }
